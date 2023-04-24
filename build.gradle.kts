cf23f64b61472d141423027308a62872b887853b
Caoimhe Byrne <caoimhebyrne06@gmail.com>
2023-04-24 21:22:48 +0100

    Bump dependency versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.6.10"
-    kotlin("plugin.serialization") version "1.6.10"
+    kotlin("jvm") version "1.8.20"
+    kotlin("plugin.serialization") version "1.8.20"
 
     `maven-publish`
 }
