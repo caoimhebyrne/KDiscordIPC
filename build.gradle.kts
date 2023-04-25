7c6f25729ef3ae25fc36e71c3c027378eadac9c4
Caoimhe <caoimhebyrne06@gmail.com>
2023-04-25 21:16:48 +0100

    Build: Downgrade to Kotlin 1.6
    One of the projects that depends on KDiscordIPC still uses Kotlin 1.6.x,
    so let's go back to that. People on newer Kotlin versions can still use
    the library, and it doesn't really affect us since we don't use any
    brand-new features.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.8.20"
-    kotlin("plugin.serialization") version "1.8.20"
+    kotlin("jvm") version "1.6.20"
+    kotlin("plugin.serialization") version "1.6.20"
 
     `maven-publish`
 }
