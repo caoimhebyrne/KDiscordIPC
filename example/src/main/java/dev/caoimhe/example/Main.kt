4037d08c1dad26806fdc8e51d7a4efe3af93cb42
Caoimhe Byrne <caoimhebyrne06@gmail.com>
2023-04-28 21:00:23 +0100

    Example: Print the user's username and discriminator on the READY event

--- example/src/main/java/dev/caoimhe/example/Main.kt
+++ example/src/main/java/dev/caoimhe/example/Main.kt
@@ -1,8 +1,15 @@
 package dev.caoimhe.example
 
 import dev.caoimhe.kdiscordipc.KDiscordIPC
+import org.apache.logging.log4j.LogManager
 
 suspend fun main() {
+    val logger = LogManager.getLogger("Example")
     val ipc = KDiscordIPC("1100847786448146505")
+
+    ipc.onReady { data ->
+        logger.info("Connected as {}#{}", data.user.username, data.user.discriminator)
+    }
+
     ipc.connect()
 }
