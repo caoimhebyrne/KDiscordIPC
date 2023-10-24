074621b3b335b81aa1bd25cf282c07e664604e1a
Caoimhe <caoimhebyrne06@gmail.com>
2023-10-24 20:52:18 +0100

    ByteToMessageDecoder: Prevent un-serializable packets from throwing

--- src/main/kotlin/dev/cbyrne/kdiscordipc/core/packet/pipeline/ByteToMessageDecoder.kt
+++ src/main/kotlin/dev/cbyrne/kdiscordipc/core/packet/pipeline/ByteToMessageDecoder.kt
@@ -7,6 +7,7 @@ import dev.cbyrne.kdiscordipc.core.socket.RawPacket
 import dev.cbyrne.kdiscordipc.core.util.json
 import kotlinx.serialization.SerializationException
 import kotlinx.serialization.decodeFromString
+import java.lang.IllegalStateException
 
 object ByteToMessageDecoder {
     fun decode(packet: RawPacket): InboundPacket? {
@@ -22,6 +23,12 @@ object ByteToMessageDecoder {
         } catch (e: SerializationException) {
             // We didn't receive the full data, probably because the socket was closed.
             throw DecodeError.InvalidData
+        } catch (e: IllegalStateException) {
+            if (e.message?.lowercase()?.contains("unknown packet command") == true) {
+                return null
+            }
+
+            throw e
         } catch (e: Exception) {
             KDiscordIPC.logger.debug(
                 "Caught error when decoding packet (op: ${packet.opcode}, length: ${packet.length})",
