3d90f903437511e2b8e118a3a2008253bfaaf006
Edouard127 <46357922+Edouard127@users.noreply.github.com>
2025-03-01 13:28:20 -0500

    Revert unix socket

--- src/main/kotlin/dev/cbyrne/kdiscordipc/core/socket/impl/UnixSocket.kt
+++ src/main/kotlin/dev/cbyrne/kdiscordipc/core/socket/impl/UnixSocket.kt
@@ -2,6 +2,10 @@ package dev.cbyrne.kdiscordipc.core.socket.impl
 
 import dev.cbyrne.kdiscordipc.core.socket.RawPacket
 import dev.cbyrne.kdiscordipc.core.socket.Socket
+import dev.cbyrne.kdiscordipc.core.util.reverse
+import org.newsclub.net.unix.AFUNIXSocket
+import org.newsclub.net.unix.AFUNIXSocketAddress
+import java.io.DataInputStream
 import java.io.File
 import java.net.UnixDomainSocketAddress
 import java.nio.ByteBuffer
@@ -9,37 +13,38 @@ import java.nio.ByteOrder
 import java.nio.channels.SocketChannel
 
 class UnixSocket : Socket {
-    private val intBuffer = ByteBuffer.allocate(4)
-        .apply { order(ByteOrder.LITTLE_ENDIAN) }
-
-    private lateinit var socket: SocketChannel
+    private val socket = AFUNIXSocket.newInstance()
 
     override val connected: Boolean
-        get() = if (::socket.isInitialized) socket.isConnected else false
+        get() = socket.isConnected
 
     override fun connect(file: File) {
-        socket = SocketChannel.open(UnixDomainSocketAddress.of(file.toPath()))
+        socket.connect(AFUNIXSocketAddress.of(file))
     }
 
     override fun read(): RawPacket {
         val opcode = readLittleEndianInt()
         val length = readLittleEndianInt()
 
-        val data = ByteBuffer.allocate(length)
-        socket.read(data)
+        val stream = DataInputStream(socket.inputStream)
+        val data = ByteArray(length)
+        stream.readFully(data)
 
-        return RawPacket(opcode, length, data.array())
+        return RawPacket(opcode, length, data)
     }
 
-    private fun readLittleEndianInt(): Int {
-        socket.read(intBuffer)
-        intBuffer.flip()
+    private fun readLittleEndianInt() =
+        ByteBuffer.wrap(readBytes(4)).int.reverse()
 
-        return intBuffer.getInt()
+    private fun readBytes(length: Int): ByteArray {
+        val array = ByteArray(length)
+        socket.inputStream.read(array, 0, length)
+
+        return array
     }
 
     override fun write(bytes: ByteArray) {
-        socket.write(ByteBuffer.wrap(bytes))
+        socket.outputStream.write(bytes)
     }
 
     override fun close() {
